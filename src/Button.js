54b81f9ea4620232453beb589c583986094c9dcf
woong <dndvud123@naver.com>
2023-07-31 23:36:31 +0900

    css 추가 및 코드 정리

---
+++ src/Button.js
@@ -0,0 +1,13 @@
+import './Button.css'
+  
+function Button({ color = 'blue', children, onClick, className = '' }) {
+    const classNames = `Button ${color} ${className}`;
+    return (
+      <button className={classNames} onClick={onClick}>
+        {children}
+      </button>
+    );
+}
+  
+  export default Button;
+  
